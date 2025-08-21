f9498eaedc4039c67198e933e77a66de3ff2f49f
David Villeda <dhernandez@layercode.com.mx>
2025-08-21 01:10:08 -0600

    Cambios

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 # Nueva forma de manejar ACL
 resource "aws_s3_bucket" "demo_bucket" {
-  bucket = "mi-bucket-demo-terraform-2027-dave"
+  bucket = "mi-bucket-demo-terraform-2077-dave"
 
   tags = {
-    Name        = "mi-bucket-demo-terraform-2027-dave"
+    Name        = "mi-bucket-demo-terraform-2077-dave"
     Environment = "dev"
   }
 }
@@ -11,4 +11,16 @@ resource "aws_s3_bucket" "demo_bucket" {
 resource "aws_instance" "example" {
   ami = "ami-0b016c703b95ecbe4"
   instance_type = "t3.small"  
+}
+
+terraform {
+  required_version = ">= 1.7.0"
+
+  backend "s3" {
+    bucket         = "mi-bucket-demo-terraform-2027-dave"
+    key            = "proyecto/terraform.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
 }
