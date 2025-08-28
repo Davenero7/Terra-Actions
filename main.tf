b29be05c04578bdf9622a897b6e62a947f2913ae
David Villeda <dhernandez@layercode.com.mx>
2025-08-28 12:54:14 -0600

    Creacion de VPC

--- main.tf
+++ main.tf
@@ -41,3 +41,70 @@ output "cyberpunk_public_ip" {
   value = module.ec2_cyberpunk.public_ip
 }
 
+########## VPC  ######
+
+
+# Crear VPC
+resource "aws_vpc" "this" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "my-vpc"
+  }
+}
+
+# Crear Subnets Públicas
+resource "aws_subnet" "public" {
+  for_each          = toset(var.public_subnets)
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = each.value
+  availability_zone = element(var.availability_zones, index(var.public_subnets, each.value))
+
+  tags = {
+    Name = "public-subnet-${each.key}"
+  }
+}
+
+# Crear Subnets Privadas
+resource "aws_subnet" "private" {
+  for_each          = toset(var.private_subnets)
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = each.value
+  availability_zone = element(var.availability_zones, index(var.private_subnets, each.value))
+
+  tags = {
+    Name = "private-subnet-${each.key}"
+  }
+}
+
+# Crear Internet Gateway
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "my-vpc-igw"
+  }
+}
+
+# Crear Route Table Pública
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+
+  tags = {
+    Name = "public-rt"
+  }
+}
+
+# Asociar Subnets Públicas a la Route Table
+resource "aws_route_table_association" "public_assoc" {
+  for_each       = aws_subnet.public
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.public.id
+}
