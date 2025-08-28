17625cf1c7183bdcae32e57326017666727c6d8e
David Villeda <dhernandez@layercode.com.mx>
2025-08-28 15:28:51 -0600

    Despliegue

--- main.tf
+++ main.tf
@@ -1,22 +1,23 @@
-# terraform {
-#   required_version = ">= 1.5.0"
-  
+terraform {
+  required_version = ">= 1.5.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
   }
-  
-#   backend "s3" {
-#     bucket         = "mi-terraform-state"
-#     key            = "terraform.tfstate"
-#     region         = "us-east-2" # Ohio
-#     dynamodb_table = "terraform-locks"
-#     encrypt        = true
-#   }
-# }
 
-# provider "aws" {
-#   region = "us-east-2"  # Ohio
-# }
+  backend "s3" {
+    bucket         = "mi-terraform-state-lc"  # <- tu bucket existente
+    key            = "terraform.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
+
+# Provider va fuera del bloque terraform
+provider "aws" {
+  region = "us-east-2"
+}
