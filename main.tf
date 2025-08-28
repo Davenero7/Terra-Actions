1814ae48509178d7f1b1674306d801e8912c1706
David Villeda <dhernandez@layercode.com.mx>
2025-08-28 14:51:49 -0600

    fix

--- main.tf
+++ main.tf
@@ -1,32 +1,22 @@
 terraform {
   required_version = ">= 1.5.0"
+  
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
   }
-
+  
   backend "s3" {
     bucket         = "mi-terraform-state"
     key            = "terraform.tfstate"
-    region         = "us-east-2"
+    region         = "us-east-2" # Ohio
     dynamodb_table = "terraform-locks"
     encrypt        = true
   }
 }
 
 provider "aws" {
-  region = "us-east-2"
-}
-
-# Llamar a los módulos
-module "vpc" {
-  source = "./vpc"
-}
-
-module "ec2" {
-  source    = "./ec2"
-  vpc_id    = module.vpc.vpc_id
-  public_subnet_id = module.vpc.public_subnet_id
+  region = "us-east-2"  # Ohio
 }
